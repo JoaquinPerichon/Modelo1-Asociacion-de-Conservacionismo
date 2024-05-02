6efecebdb433fbdd4d606e367e765d424db9a3c1
Perichón Guglielmone Joaquín <145691417+JoaquinPerichon@users.noreply.github.com>
2024-05-02 07:10:21 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+//     var altura = $('.navbar').offset().top;
+
+//     $(window).on('scroll', function(){
+//     if($(window).scrollTop() > altura){
+//         $('.navbar').addClass('navbar-fixed');
+//     }else{
+//         $('.navbar').removeClass('navbar-fixed');
+//     }
+//    });
+// });
